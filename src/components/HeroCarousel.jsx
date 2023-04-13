dfe5c55229596a42604355ad6d4eaf255de9fd04
RUSHWAYC <rushwayc@yahoo.com>
2023-04-14 00:33:13 +0200

    Fixed carousel. Img1 height broken.

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -50,44 +50,53 @@ const HeroCarousel = () => {
   };
 
   return (
-    <div className="relative h-screen">
+    <div className="relative">
       {images.map((image, index) => (
         <div
           key={index}
-          className={`absolute inset-0 w-full h-full transition-opacity duration-500 ${
-            index === currentImage ? "opacity-100" : "opacity-0"
+          className={`w-full transition-opacity duration-500 ${
+            index === currentImage
+              ? "opacity-100"
+              : "opacity-0 absolute top-0 left-0"
           }`}
         >
-          <img src={image.src} alt={image.alt} className="w-full h-full" />
+          <img
+            src={image.src}
+            alt={image.alt}
+            className="w-full h-full object-cover"
+          />
           <div className="absolute inset-0 flex flex-col justify-center items-center">
             <h1 className="text-4xl font-bold text-white mb-4">
               {image.title}
             </h1>
             <p className="text-lg text-white">{image.subtitle}</p>
           </div>
-          <div className="absolute inset-0 flex justify-center items-center">
-            <button
-              onClick={prevSlide}
-              className="absolute left-0 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-gray-800 text-white rounded-full shadow-lg focus:outline-none"
-            >
-              <MdOutlineKeyboardArrowLeft className="h-6 w-6" />
-            </button>
-            <button
-              onClick={nextSlide}
-              className="absolute right-0 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-gray-800 text-white rounded-full shadow-lg focus:outline-none"
-            >
-              <MdOutlineKeyboardArrowRight className="h-6 w-6" />
-            </button>
-          </div>
         </div>
       ))}
-      <div className="absolute bottom-0 left-0 w-full flex justify-center pb-8">
+      <div className="absolute inset-0 flex items-center justify-between">
+        <button
+          onClick={prevSlide}
+          className="absolute left-0 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-gray-800 text-white rounded-full shadow-lg focus:outline-none"
+        >
+          <MdOutlineKeyboardArrowLeft className="h-6 w-6" />
+        </button>
+        <button
+          onClick={nextSlide}
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-gray-800 text-white rounded-full shadow-lg focus:outline-none"
+        >
+          <MdOutlineKeyboardArrowRight className="h-6 w-6" />
+        </button>
+      </div>
+      <div className="absolute bottom-0 left-0 w-full flex justify-center mb-8">
         {images.map((image, index) => (
           <button
             key={index}
-            onClick={() => setCurrentImage(index)}
+            onClick={() => {
+              setCurrentImage(index);
+              setActiveDotIndex(index);
+            }}
             className={`${
-              index === currentImage ? "bg-white" : "bg-gray-400"
+              index === activeDotIndex ? "bg-white" : "bg-gray-400"
             } h-2 w-2 mx-2 rounded-full focus:outline-none`}
           ></button>
         ))}
