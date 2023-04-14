e5a9ecf6287f2496cceaa155091dd2d47a16fcb3
RUSHWAYC <rushwayc@yahoo.com>
2023-04-14 14:37:47 +0200

    Started TextBox component.

---
+++ src/components/TextBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TextBox = ({ title, subt1, subt2 }) => {
+  return (
+    <div className="border-l-2 border-r-2 border-t-2 border-gray-300 mx-20 my-4 p-4">
+      <h1 className="text-4xl font-bold mb-4">{title}</h1>
+      <p className="mb-2">{subt1}</p>
+      <p>{subt2}</p>
+    </div>
+  );
+};
+
+export default TextBox;
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import HeroCarousel from "../components/HeroCarousel";
+import TextBox from "../components/TextBox";
 
 const Home = () => {
   return (
     <div>
       {/* <HeroSlider /> */}
       <HeroCarousel />
+      <TextBox
+        title="Primena naših proizvoda"
+        subt1="Kroz brojne saradnje razvijene tokom godina sa renomiranim proizvođačima, proizvodi koje distribuira Grosvenor SCM zadovoljavaju i najzahtevnije potrebe klijenata iz elektronske, elektrotehničke, automobilske, tekstilne i plastične industrije, kao i iz industrije obrade metala."
+        subt2="Kompletna rešenja za proizvodnju štampanih ploča i elektronskih kola, čišćenje i tretman površina, zaštitno antistatičko pakovanje i obeležavanje, rešenja za ličnu zaštitu su deo visokokvalitetnih proizvoda i usluga koji se isporučuju u najkraćem roku."
+      />
     </div>
   );
 };
