6a2f9ce9318522a61feae3afd7cadc26602ffc91
RUSHWAYC <rushwayc@yahoo.com>
2023-04-28 13:14:44 +0200

    Services.jsx done.

--- src/components/ImageBanner.jsx
+++ src/components/ImageBanner.jsx
@@ -3,12 +3,16 @@ import React from "react";
 const ImageBanner = ({ src, alt, title, subtitle }) => {
   return (
     <div className="relative">
-      <img src={src} alt={alt} className="w-full object-cover" />
-      <div className="absolute bottom-0 left-0 bg-white bg-opacity-75 py-4 px-6">
-        <h2 className="text-2xl font-bold mb-2">{title}</h2>
-        <div className="h-20 overflow-y-auto scrollbar-thumb-gray-300 scrollbar-track-gray-100 hover:scrollbar-thumb-gray-400 hover:scrollbar-track-gray-200">
-          <p className="text-lg">{subtitle}</p>
+      <div className="h-1/2 sm:h-3/5" style={{ height: "200px" }}>
+        <div className="absolute top-0 left-0 w-full h-full">
+          <img src={src} alt={alt} className="w-full h-full object-cover" />
+          <div className="absolute top-0 left-0 bg-sky-500 text-white py-2 px-4 mx-10 lg:mx-16 mt-4">
+            <h2 className="text-xl sm:text-3xl font-bold mb-2">{title}</h2>
+          </div>
         </div>
+      </div>
+      <div className="absolute bottom-0 left-0 bg-sky-500 text-white py-4 px-6 mx-10 lg:mx-16 transform translate-y-1/2">
+        <p className="text-md sm:text-lg">{subtitle}</p>
       </div>
     </div>
   );
--- src/container/Services.jsx
+++ src/container/Services.jsx
@@ -6,16 +6,14 @@ import slides from "../data/slideData";
 const Services = () => {
   return (
     <>
-      <div className="mb-0 md:mb-16 lg:mb-40">
-        <div className="h-1/2 sm:h-3/5" style={{ height: "200px" }}>
-          <ImageBanner
-            src={slides[1].src}
-            alt={slides[1].alt}
-            title={slides[1].title}
-            subtitle={slides[1].subtitle}
-            className="w-full h-full object-cover"
-          />
-        </div>
+      <div className="mb-20 md:mb-14 lg:mb-10">
+        <ImageBanner
+          src={slides[1].src}
+          alt={slides[1].alt}
+          title={slides[1].title}
+          subtitle={slides[1].subtitle}
+          className="w-full h-full object-cover"
+        />
       </div>
       <div>
         <ServicesGrid subtitle="True" />
